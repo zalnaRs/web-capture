2e3b26003454e1e3ea3ebb611617332fe7b5fb66
zalnaRs <zalnars@proton.me>
2025-09-12 20:31:19 +0200

    feat: add recording context and cards

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { mount } from 'ripple';
 // @ts-expect-error: known issue, we're working on it
 import { App } from './App.ripple';
 
+import './style.css'
+
 mount(App, {
 	target: document.getElementById('root'),
 });
