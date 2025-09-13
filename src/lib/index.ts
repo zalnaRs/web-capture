c46a412f71fc73094b6bab9902d799953d3ae32f
zalnaRs <zalnars@proton.me>
2025-09-13 09:16:31 +0200

    feat: add recording

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,42 @@
 let recordedChunks = [];
+let startTime: Number;
 
-const startPreview = async (stream: MediaStream) => {
-	const mediaRecorder = new MediaRecorder(stream);
-	mediaRecorder.start();
+let mediaRecorder: MediaRecorder;
+
+const startRecording = async (stream: MediaStream) => {
+	startTime = new Date().getTime();
+
+	mediaRecorder = new MediaRecorder(stream);
 
 	mediaRecorder.ondataavailable = (ev) => {
-		recordedChunks.push(ev.data);
+		if (ev.data.size > 0) {
+			recordedChunks.push(ev.data);
+		}
 	};
+
+	mediaRecorder.onstop = () => {
+		const url = URL.createObjectURL(new Blob(recordedChunks, { type: mediaRecorder.mimeType }));
+
+		mediaRecorder.stop();
+
+		const a = document.createElement('a');
+		a.download = 'video-stream.webm';
+		a.href = url;
+		a.style.display = 'none';
+		document.body.append(a);
+		a.click();
+		a.remove();
+
+		window.URL.revokeObjectURL(url);
+		mediaRecorder = undefined;
+		recordedChunks = [];
+	};
+
+	mediaRecorder.start();
+};
+
+const stopRecording = async () => {
+	mediaRecorder.stop();
 };
 
 const selectSource = async (): Promise<MediaStream> => {
@@ -21,7 +51,11 @@ const selectSource = async (): Promise<MediaStream> => {
 
 const RecordingAPI = {
 	selectSource,
-	startPreview,
+	startRecording,
+	stopRecording,
+	properties: {
+		startTime,
+	},
 };
 
 export default RecordingAPI;
