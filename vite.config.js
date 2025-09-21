1cc28bcffef9e8d683388e0e904250d7131fee05
zalnaRs <zalnars@proton.me>
2025-09-21 21:05:35 +0200

    feat: add service worker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import { ripple } from 'vite-plugin-ripple';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [ripple()],
+	plugins: [VitePWA({ registerType: 'autoUpdate' }), ripple()],
 	server: {
 		port: 3000,
 	},
